921712838d08c60c0678bd760ca27c27be277474
Johan Westlund <johanwestlund_100@hotmail.com>
2012-10-05 17:02:34 +0200

    Update

--- webcam.c
+++ webcam.c
@@ -3,7 +3,8 @@
 int main(void)
 {
   int hCam = -1;
-  struct v4l2_queryctrl ctrl;
+  struct ImageSize size;
+  //struct v4l2_queryctrl ctrl;
 
   if(!initCam(&hCam)){
     printf("Could not open device\n");
@@ -11,11 +12,21 @@ int main(void)
   }
 
   if(!hasVideoCaptureCapability(hCam)){
-    printf("Device has not video capture capabilities\n");
+    printf("Device doesn't have video capture capabilities\n");
     return -1;
   }
+ 
+  size = getImageSize(hCam);
 
-  ioctl(hCam, VIDIOC_QUERYCTRL, &ctrl);
+  /*
+    y1   = yuv[0];
+    u    = yuv[1];
+    y2   = yuv[2];
+    v    = yuv[3];
+
+    rgb1 = YUV444toRGB(y1, u, v);
+    rgb2 = YUV444toRGB(y2, u, v);
+  */
   
   if(!disposeCam(hCam)){
     printf("Could not close device\n");
@@ -47,7 +58,45 @@ bool hasVideoCaptureCapability(int hCam)
   struct v4l2_capability camCap;
   memset(&camCap, 0, sizeof(camCap));
   ioctl(hCam, VIDIOC_QUERYCAP, &camCap);
-  printf("Capabilities: %x\n", camCap.capabilities);
   return camCap.capabilities & V4L2_CAP_VIDEO_CAPTURE ? true : false;
 }
  
+struct ImageSize getImageSize(int hCam)
+{
+  struct v4l2_format format;
+  struct ImageSize imageSize;
+  
+  format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+  ioctl(hCam, VIDIOC_G_FMT, &format);  
+  
+  imageSize.width = format.fmt.pix.width;
+  imageSize.height = format.fmt.pix.height;
+        
+  return imageSize;
+}
+
+struct RGB YUV444toRGB(u8 y, u8 u, u8 v)
+{
+  int a, b, c;
+  struct RGB rgb;
+  
+  a = y - 16;
+  b = u - 128;
+  c = v - 128;
+
+  rgb.R = clamp((298*a + 409*c + 128) >> 8);
+  rgb.G = clamp((298*a - 100*b - 208*c + 128) >> 8);
+  rgb.B = clamp((298*a + 516*b + 128) >> 8);
+
+  return rgb;
+}
+
+u8 clamp(int val)
+{
+  if(val < 0) val = 0;
+  if(val > 255) val = 255;
+  return (u8)val;
+}
+
+
--- webcam.h
+++ webcam.h
@@ -13,10 +13,33 @@
 
 static const char * deviceName = "/dev/video0";
 
+typedef unsigned int u32;
+typedef unsigned char u8;
 typedef enum {false,true}bool;
+
+struct ImageSize{
+  u32 width;
+  u32 height;
+};
+
+struct RGB{
+  u8 R;
+  u8 G;
+  u8 B;
+};
+
+struct YUV{
+  u8 Y;
+  u8 U;
+  u8 V;
+};    
 
 bool initCam(int * hCam);
 bool disposeCam(int hCam);
 bool hasVideoCaptureCapability(int hCam);
+struct ImageSize getImageSize(int hCam);
+struct RGB YUV444toRGB(u8 y, u8 u, u8 v);
+u8 clamp(int val);
+
 
 
